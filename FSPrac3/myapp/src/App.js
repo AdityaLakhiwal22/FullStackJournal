533ec5107e67d7705010cb603486da93a04eb7c4
AdityaLakhiwal22 <adityalakhiwal@gmail.com>
2025-07-26 15:50:56 +0530

    Add files via upload

---
+++ FSPrac3/myapp/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState, useEffect } from 'react';
+
+function fetchUsers(){
+  return fetch('https://jsonplaceholder.typicode.com/users').then(response => {
+    if (!response.ok){
+      throw new Error("Network response was not ok");
+    }
+      return response.json();
+    });
+  }
+function App() {
+  const [users, setUsers] = useState({});
+  useEffect(() => {
+    fetchUsers().then((data) => setUsers(data));
+  }, []);
+  if (!users.length){
+    return <div>Loading.....</div>
+  }
+  if(users.length){
+    return <div>
+      {users.map((user) => (
+        <div className='p-4 bg-white shadow-md mt-4' key = {user.id}>
+          <h2>{user.name}</h2>
+          <p>Email: {users.email}</p>
+          <p>Phone: {user.phone}</p>
+          <p>Website: {user.website}</p>
+          <p>Company: {user.company.name}</p>
+          </div>
+      ))}
+    </div>
+  }
+  
+}
+
+export default App;
